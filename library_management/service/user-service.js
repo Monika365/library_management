658e50631cebc4008233feefb5fe6676278231fd
“Monika365” <shahmonika118@gmail.com>
2021-08-26 19:48:19 +0530

    commit

--- library_management/service/user-service.js
+++ library_management/service/user-service.js
@@ -1,11 +1,10 @@
 const UserDAO = require('../dao/user-dao');
-const utils = require('../utils/utility');
+
 const UserService = {
 
     storeUser: (payload) => {
         return new Promise((resolve, reject) => {
-            let userId = utils.getUserId(payload.role, 4);
-            payload['userId']= userId;
+           
             UserDAO.storeUser(payload).then((result) => {
                 resolve(result);
             }).catch(error => {
